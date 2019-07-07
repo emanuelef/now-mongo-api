28879ed59cc5441aef6ac4392615e4538fb1dab6
Emanuele Fumagalli <emanuelef@gmail.com>
2019-07-07 18:06:32 +0100

    added query params

--- api/allFlights.js
+++ api/allFlights.js
@@ -24,16 +24,30 @@ async function connectToDatabase(uri) {
 module.exports = async (req, res) => {
   // Get a database connection, cached or otherwise,
   // using the connection string environment variable as the argument
+
+  const url_parts = url.parse(req.url, true);
+  const query = url_parts.query;
+
+  /*
+  if (!("start" in query && "end" in query)) {
+    res.status(400);
+    return;
+  }
+  */
+
   const db = await connectToDatabase(process.env.MONGODB_URI);
 
   const collection = await db.collection("flights");
 
   const end = Math.floor(Date.now() / 1000);
-  const start = Math.floor(Date.now() / 1000) - 60 * 60;
+  const start = Math.floor(Date.now() / 1000) - 24 * 60 * 60;
+
+  console.log(query.start, query.end);
+  console.log(start, end);
 
   // Select the users collection from the database
   const results = await collection
-    .find({ startTime: { $gte: start, $lt: end } })
+    .find({ startTime: { $gte: Number(query.start), $lt: Number(query.end) } })
     .toArray();
 
   res.setHeader("Content-Type", "application/json");
