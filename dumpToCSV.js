d4db77a05e51a99cc1923d8ed66c8fcd8b530ae4
Emanuele Fumagalli <emanuelef@gmail.com>
2019-10-15 00:06:04 +0100

    working on script to dump to csv

---
+++ dumpToCSV.js
@@ -0,0 +1,84 @@
+const MongoClient = require("mongodb").MongoClient;
+const url = require("url");
+
+const MONGODB_URI =
+  "mongodb://admin:ashbeck19@ds343887.mlab.com:43887/lhr-passages";
+
+let cachedDb = null;
+
+const getLastString = el =>
+  el
+    ? el
+        .split(",")
+        .pop()
+        .trim()
+    : "";
+
+async function connectToDatabase(uri) {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  const db = await client.db(url.parse(uri).pathname.substr(1));
+
+  // Cache the database connection and return the connection
+  cachedDb = db;
+  return db;
+}
+
+async function dump() {
+  const db = await connectToDatabase(MONGODB_URI);
+  const collection = await db.collection("flights");
+  const end = Math.floor(Date.now() / 1000);
+  const start = Math.floor(Date.now() / 1000) - 24 * 60 * 60;
+
+  const results = await collection.find({
+    startTime: { $gte: Number(start), $lt: Number(end) }
+  });
+
+  cont = 0;
+  results.forEach(doc => {
+    const {
+      op,
+      from,
+      to,
+      startTime,
+      startLat,
+      startLon,
+      startAltitude,
+      speedAtCreation,
+      verticalSpeedAtCreation,
+      wakeTurbulence,
+      wDeg,
+      wSpeed,
+      minDistance,
+      ...partialObject
+    } = doc;
+    const subset = {
+      op,
+      from,
+      to,
+      startTime,
+      startLat,
+      startLon,
+      startAltitude,
+      speedAtCreation,
+      verticalSpeedAtCreation,
+      wakeTurbulence,
+      wDeg,
+      wSpeed,
+      minDistance
+    };
+
+    subset.from = getLastString(subset.from);
+    subset.to = getLastString(subset.to);
+
+    console.log(cont++, subset);
+  });
+}
+
+dump();
